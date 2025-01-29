e3fad9faf9128ce9a46b7cd84c8a116f5813d02d
shammysha <65885873+shammysha@users.noreply.github.com>
2025-01-29 16:52:16 +0300

    Update dlms_cosem_sensor.h

--- components/dlms_cosem/dlms_cosem_sensor.h
+++ components/dlms_cosem/dlms_cosem_sensor.h
@@ -113,8 +113,8 @@ class DlmsCosemTextSensor : public DlmsCosemSensorBase, public text_sensor::Text
     bool has_got_scale_and_unit() override { return true; }
 
     void set_value(const char *value) {
-      char res[sizeof(&value)]; 
-      cp1251_to_utf8((char*) &res, (char*) &value);
+      char res[sizeof(&value)*3+1]; 
+      cp1251_to_utf8((char*) &res, value);
       value_ = std::string(res);
       has_value_ = true;
       tries_ = 0;
