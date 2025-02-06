109a6037444838052e8116d7c3d0f8b9403b1477
shammysha <65885873+shammysha@users.noreply.github.com>
2025-02-06 09:34:54 +0300

    Some fixes

--- components/dlms_cosem/dlms_cosem.h
+++ components/dlms_cosem/dlms_cosem.h
@@ -150,6 +150,7 @@ class DlmsCosemComponent : public PollingComponent, public uart::UARTDevice {
 
   void indicate_transmission(bool transmission_on);
   void indicate_session(bool session_on);
+  void indicate_connection(bool connection_on);
 
 
   // void read_reply_and_go_next_state_(ReadFunction read_fn, State next_state, uint8_t retries, bool mission_critical,
